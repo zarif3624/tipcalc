eaa7e9e254c92a454552a003b4e161ccf68a52dd
zarif3624 <zarif.choudhury@yahoo.com>
2019-01-29 09:38:43 -0600

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Zarif Choudhury on 1/29/19.
+//  Copyright © 2019 Zarif Choudhury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
